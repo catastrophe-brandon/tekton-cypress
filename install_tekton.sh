96aad92647767ae4e45627357dfc190972e3620e
Tweed <btweed@redhat.com>
2025-10-08 14:50:36 -0500

    Progress

--- install_tekton.sh
+++ install_tekton.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Install Tekton and required tasks to execute the pipeline
+# TODO: confirm kubectl and tkn are installed and functional
+set -e
+
 # Install Tekton to an existing minikube instance
 # install tekton
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
@@ -7,3 +11,5 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline
 # This will show `1/1` under the READY column once tekton is available
 kubectl get pods --namespace tekton-pipelines --watch
 
+# Install the git-clone task for use in the pipeline
+tkn hub install task git-clone
