afa8b33ec366f1a31c605df04fd65e4951920950
Tweed <btweed@redhat.com>
2025-10-08 21:50:43 -0500

    Add E2E user creds as parameters to PipelineRun

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [ ! -n "${E2E_USER}" ]; then
+	echo "Set E2E_USER"
+	exit 1
+fi
+
+if [ ! -n "${E2E_PASSWORD}" ]; then
+	echo "Set E2E_PASSWORD"
+	exit 1
+fi
 
 echo "Clearing out previous task run (if present)"
 yes | tkn taskrun delete e2e-task-run
