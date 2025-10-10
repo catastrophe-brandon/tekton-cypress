1bd96afc28f40ecc6abf91c7be5883fda4ba8102
Tweed <btweed@redhat.com>
2025-10-10 14:56:13 -0500

    Update image load script

--- image_load.sh
+++ image_load.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 
-# For whatever reason, minikube has issues pulling the cypress image and some others
-# This script explicitly pulls them with podman and then loads them into minikube
+# minikube has issues pulling the cypress image and some others due to excessive rate limiting by docker.io
+# This script explicitly pulls them with podman and then loads them into minikube to sidestep docker's aggressive limitations
 #
 set -e
-podman pull cypress/included:latest
-minikube image load cypress/included:latest
+
+# If you are not logged into quay.io with podman, this will not work
+podman pull docker.io/cypress/included:latest
+podman push $(podman images cypress -q) quay.io/btweed/cypress
+podman pull quay.io/btweed/cypress:latest
+minikube image load quay.io/btweed/cypress:latest
 
 # TODO: add code to pull the minikube dashboard images and load them into the cluster
 
